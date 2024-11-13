11a2eb0f8efb186d248b4347fb1ec4f46b81f08c
fabio_saupp <fabio.sa@outlook.de>
2024-11-13 11:42:20 +0100

    enabled cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { PORT } from './env';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   //v1, v2...
   app.enableVersioning({
     type: VersioningType.URI,
